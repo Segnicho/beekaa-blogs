13b9b392d7bfe0cb19375c82f71a109704fc6283
Segnicho <segnidesta60@gmail.com>
2022-10-23 16:51:22 +0300

    adding the frontend design impl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import Home from './pages/home/Home'
+import Topbar from './components/topbar/Topbar'
+// import Single from './pages/single/Single'
+import Write from './pages/write/Write'
+import Settings from './pages/settings/Settings'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import { BrowserRouter as Router,Routes, Route  } from 'react-router-dom';
+
 function App() {
+     const user = false;
   return (
-    <div >
-      <h1>
-        App
-      </h1>
-    </div>
-  );
+       <Router>
+          <Topbar />
+     <Routes>
+     <Route exact path="/" element={<Home/>} />
+     <Route path="/register" element={user? <Home/>: <Register/>} />
+     <Route path="/login" element={user? <Home />: <Login/>} />
+     <Route path="/post" element={user? <Write/>: <Login />} />
+     <Route path="/settings" element={user? <Settings/> : <Login/>} />
+     </Routes>
+</Router>
+     )
 }
 
-export default App;
+export default App
