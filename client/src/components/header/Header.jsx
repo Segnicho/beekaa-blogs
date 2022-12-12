b6c8bde7a97ac973c77926c94c84501c74ff9fd0
Segnicho <segnidesta60@gmail.com>
2022-12-12 16:55:20 +0300

    some changes to the client side

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import "./header.css"
+import blogger from '../../assets/blogger.jpeg'
 function Header() {
   return (
 <div className="header">
@@ -7,6 +8,7 @@ function Header() {
         <span className="headerTitleLg"> Beekaa Blogs </span>
     </div>
     <img className="headerImg" src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80" alt="background" />
+    {/* <img className="headerImg" src={blogger} alt="background" /> */}
 </div>
     )
 }
